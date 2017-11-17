a174849908b0d0ee7a5095bc9fe08f9e8eeb629a
vontell <vontell@mit.edu>
2017-11-16 21:34:20 -0500

    Initial Commit

---
+++ Sportball/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Sportball
+//
+//  Created by Aaron Vontell on 11/16/17.
+//  Copyright © 2017 Vontech Software, LLC. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Sportball/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Sportball
+//
+//  Created by Aaron Vontell on 11/16/17.
+//  Copyright © 2017 Vontech Software, LLC. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
